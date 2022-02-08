896eb67996a14d1700d1145a4e64d4b7775333d5
muhpadli <muhp5934@gmail.com>
2022-02-08 07:38:21 +0700

    my web

---
+++ galeri.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>web profile</title>
+</head>
+
+<body id="GALERI" style="background-color: #b2caee;">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent  fixed-top">
+        <div class="container">
+            <a class="navbar-brand text-light" href="#">MUH. PADLI</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse text-right" id="navbarText">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-light active" aria-current="page" href="index.html">BERANDA</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="galeri.php">GALERI</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="tentang.php">TENTANG</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#kontak">KONTAK</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!--Banner-->
+    <div class="container-fluid banner">
+        <div class="container text-center text-white">
+            <img src="to/padl17i.jpg" alt="Muh. Padli" width="200" height="200" class="rounded-circle img-thumbnail" />
+            <h2 class="display-3"> MUH. PADLI</h2>
+            <p class="lead">Mahasiswa | Programmer</p>
+            <a href="galeri.php">
+                <button type="button" class="btn btn-lg text-light rounded-pill" style="background-color: #6f42c1;">
+                    KUNJUNGI
+                </button>
+            </a>
+        </div>
+    </div>
+
+    <div class="container-fluid pt-5 pb-5 galeri">
+        <div class="container text-center">
+            <h3 class="display-4  text-light">GALERI</h3>
+            <hr class="mb-5">
+            <div class="row row-cols-1 row-cols-md-3 g-3">
+                <div class="col">
+                    <div class="card">
+                        <img src="to/osp.jpg" class="card-img-top" alt="peserta osp">
+                        <div class="card-body">
+                            <p class="card-text">Peserta Olimpiade Sains tingkat Provinsi asal sekolah SMA NEGERI 1 PAMBOANG </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card">
+                        <img src="to/kknuin66.jpg" class="card-img-top" alt="kknuin">
+                        <div class="card-body">
+                            <p class="card-text">Foto bersama salah satu mahasiswa KKKN UINAM angk 66 dengan remaja masjid jami sirajuttaliin Sirindu pada hari penarikan KKN UINAM angk. 66 tahun 2021 </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card">
+                        <img src="to/siswa.jpg" class="card-img-top" alt="kelas 11">
+                        <div class="card-body">
+                            <p class="card-text">Foto bersama siswa kelas XI IPA 1 SMAN 1 Pamboang pada peringatan hari PGRI</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="hsl(216, 94%, 67%)" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+        </path>
+    </svg>
+    <footer style="background-color: hsl(216, 94%, 67%);" class="text-light pt-3 pb-2">
+        <p class="text-center ">Created With <i class="bi bi-heart-fill text-danger"></i> by <a href="https://wa.me/6283113955334" class="link-warning ">Padli_17</a>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ tentang.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>web profile</title>
+</head>
+
+<body id="tentang" style="background-color: #b2caee;">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent  fixed-top">
+        <div class="container">
+            <a class="navbar-brand text-light" href="#">MUH. PADLI</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse text-right" id="navbarText">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-light active" aria-current="page" href="index.html">BERANDA</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="galeri.php">GALERI</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="tentang.php">TENTANG</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#kontak">KONTAK</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!--Banner-->
+    <div class="container-fluid banner">
+        <div class="container text-center text-white">
+            <img src="to/padl17i.jpg" alt="Muh. Padli" width="200" height="200" class="rounded-circle img-thumbnail" />
+            <h2 class="display-3"> MUH. PADLI</h2>
+            <p class="lead">Mahasiswa | Programmer</p>
+            <a href="galeri.php">
+                <button type="button" class="btn btn-lg text-light rounded-pill" style="background-color: #6f42c1;">
+                    KUNJUNGI
+                </button>
+            </a>
+        </div>
+    </div>
+
+    <div class="container text-center pt-5 tentang">
+        <h3 class="display-4 text-light ">TENTANG</h3>
+        <hr class="mb-5 ">
+        <div class="row justify-content-around border col-md-10 offset-1 bg-light pb-5 rounded-3">
+            <div class="col-md-6 offset-1">
+                <h6 class="text-start pt-5 pb-2">DATA DIRI</h6>
+                <table class="text-start" width="450">
+                    <tr>
+                        <td>NAMA</td>
+                        <td>: MUH. PADLI</td>
+                    </tr>
+                    <tr>
+                        <td>NIM</td>
+                        <td>: D0221513</td>
+                    </tr>
+                    <tr>
+                        <td>JENIS KELAMIN</td>
+                        <td>: LAKI - LAKI</td>
+                    </tr>
+                    <tr>
+                        <td>TTL</td>
+                        <td>: SIRINDU, 24 JANUARI 2003</td>
+                    </tr>
+                    <tr>
+                        <td>ALAMAT</td>
+                        <td>: SIRINDU, KEC. PAMBOANG, KAB. MAJENE</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">STATUS</td>
+                        <td>: MAHASISWA</td>
+                    </tr>
+                    <tr>
+                        <td>AGAMA</td>
+                        <td>: ISLAM</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="col-md-5">
+                <h6 class="text-start pt-5 pb-2">JENJANG PENDIDIKAN</h6>
+                <table class="text-start">
+                    <tr>
+                        <td>2009-2015</td>
+                        <td>: SD 23 INPRES SIRINDU</td>
+                    </tr>
+                    <tr>
+                        <td>2015-2018</td>
+                        <td>: SMP NEGERI 2 PAMBOANG</td>
+                    </tr>
+                    <tr>
+                        <td>2018-2021</td>
+                        <td>: SMA NEGERI 1 PAMBOANG</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="hsl(216, 94%, 67%)" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+        </path>
+    </svg>
+    <footer style="background-color: hsl(216, 94%, 67%);" class="text-light pt-3 pb-2">
+        <p class="text-center ">Created With <i class="bi bi-heart-fill text-danger"></i> by <a href="https://wa.me/6283113955334" class="link-warning ">Padli_17</a>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+
+</html>
